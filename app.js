8f732e492e5c15542b7a39bb64dac0fef8fa28a3
Sagar Thakkar <sthakkar42@gmail.com>
2020-05-19 15:06:01 -0500

    Retrieve data from weather Api and update current Weather to DOM

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ function getlocation(lati, long) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&units=metric&appid=${key}`)
         .then(resp => resp.json())
         .then(data => {
+            currentWeather(data);
             console.log(data);
         })
+}
+
+function currentWeather(data) {
+    const currentConditionEle = document.querySelector(".current-conditions");
+    currentConditionEle.innerHTML = `
+
+          
+          <h2>Current Conditions</h2>
+          <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+          <div class="current">
+            <div class="temp">${parseInt(data.main.temp)}℃</div>
+            <div class="condition">${data.weather[0].description}</div>
+          </div>
+        </div>`
+
+
 }
